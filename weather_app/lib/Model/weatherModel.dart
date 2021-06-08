7d0ed16152300f2992ca7f82e56f523031c42340
jigyasa1411 <jigyasa.chaudhary@successive.tech>
2021-06-08 07:28:19 +0530

    Weather Page using API

---
+++ weather_app/lib/Model/weatherModel.dart
@@ -0,0 +1,22 @@
+class WeatherModel {
+  String cityName;
+  String countryName;
+  String highTemp;
+  String lowTemp;
+  String temperature;
+  String cloud;
+  String humidity;
+  String windSpeed;
+  String weather;
+
+  WeatherModel(
+      {this.cityName,
+      this.countryName,
+      this.highTemp,
+      this.lowTemp,
+      this.temperature,
+      this.cloud,
+      this.humidity,
+      this.windSpeed,
+      this.weather});
+}

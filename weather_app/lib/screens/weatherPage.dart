886b7145494c271ac44890c159071518639535e7
jigyasa1411 <jigyasa.chaudhary@successive.tech>
2021-06-08 07:47:22 +0530

    Layout Changes

--- weather_app/lib/screens/weatherPage.dart
+++ weather_app/lib/screens/weatherPage.dart
@@ -46,130 +46,137 @@ class _WeatherPageState extends State<WeatherPage> {
       builder: (context, snapshot) {
         if (snapshot.data != null) {
           return Scaffold(
-            body: Container(
-                padding: EdgeInsets.only(left: 20, right: 20, top: 30),
-                height: MediaQuery.of(context).size.height,
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    fit: BoxFit.fill,
-                    image: AssetImage("assets/morning.jpg"),
+            body: SafeArea(
+              child: Container(
+                  padding: EdgeInsets.only(left: 20, right: 20, top: 30),
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      fit: BoxFit.fill,
+                      image: AssetImage("assets/morning.jpg"),
+                    ),
                   ),
-                ),
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        SvgPicture.asset("assets/cloud.svg",
-                            color: Color.fromRGBO(226, 243, 246, 1),
-                            height: 50,
-                            width: 50),
-                        Text(_response.clouds.all.toString() + "%",
-                            style: TextStyle(
-                                color: Color.fromRGBO(226, 243, 246, 1),
-                                fontSize: 20)),
-                        Spacer(),
-                        SvgPicture.asset("assets/wind.svg",
-                            color: Color.fromRGBO(226, 243, 246, 1),
-                            height: 50,
-                            width: 50),
-                        Text(_response.wind.speed.toString() + " m/s",
-                            style: TextStyle(
-                                color: Color.fromRGBO(226, 243, 246, 1),
-                                fontSize: 20)),
-                        Spacer(),
-                        SvgPicture.asset("assets/humidity.svg",
-                            color: Color.fromRGBO(226, 243, 246, 1),
-                            height: 50,
-                            width: 50),
-                        Text(_response.main.humidity.round().toString() + " %",
-                            style: TextStyle(
-                                color: Color.fromRGBO(226, 243, 246, 1),
-                                fontSize: 20)),
-                      ],
-                    ),
-
-                    /***COLUMN Started */
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          _response.main.temp.round().toString(),
-                          style: TextStyle(
-                              fontSize: 150,
-                              color: Color.fromRGBO(226, 243, 246, 1)),
-                        ),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            Text(
-                              "\u00b0C",
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          SvgPicture.asset("assets/cloud.svg",
+                              color: Color.fromRGBO(226, 243, 246, 1),
+                              height: 50,
+                              width: 50),
+                          Text(_response.clouds.all.toString() + "%",
                               style: TextStyle(
-                                  fontSize: 40,
-                                  color: Color.fromRGBO(226, 243, 246, 1)),
-                            ),
-                            SizedBox(height: 5),
-                            Row(
-                              children: [
-                                SvgPicture.asset("assets/high.svg",
-                                    color: Color.fromRGBO(226, 243, 246, 1),
-                                    height: 20,
-                                    width: 20),
-                                Text(
-                                  _response.main.tempMax.round().toString() +
-                                      "\u00b0C",
-                                  style: TextStyle(
-                                      fontSize: 15,
-                                      color: Color.fromRGBO(226, 243, 246, 1)),
-                                ),
-                              ],
-                            ),
-                            Row(
-                              children: [
-                                SvgPicture.asset("assets/low.svg",
-                                    color: Color.fromRGBO(226, 243, 246, 1),
-                                    height: 20,
-                                    width: 20),
-                                Text(
-                                  _response.main.tempMin.round().toString() +
-                                      "\u00b0C",
-                                  style: TextStyle(
-                                      fontSize: 15,
-                                      color: Color.fromRGBO(226, 243, 246, 1)),
-                                ),
-                              ],
-                            )
-                          ],
-                        )
-                      ],
-                    ),
+                                  color: Color.fromRGBO(226, 243, 246, 1),
+                                  fontSize: 20)),
+                          Spacer(),
+                          SvgPicture.asset("assets/wind.svg",
+                              color: Color.fromRGBO(226, 243, 246, 1),
+                              height: 50,
+                              width: 50),
+                          Text(_response.wind.speed.toString() + "m/s",
+                              style: TextStyle(
+                                  color: Color.fromRGBO(226, 243, 246, 1),
+                                  fontSize: 20)),
+                          Spacer(),
+                          SvgPicture.asset("assets/humidity.svg",
+                              color: Color.fromRGBO(226, 243, 246, 1),
+                              height: 50,
+                              width: 50),
+                          Text(_response.main.humidity.round().toString() + "%",
+                              style: TextStyle(
+                                  color: Color.fromRGBO(226, 243, 246, 1),
+                                  fontSize: 20)),
+                        ],
+                      ),
+                      SizedBox(height: 30),
 
-                    /******COLUMN */
-                    Column(
-                      children: [
-                        Text(
-                          _response.name + "," + _response.sys.country,
-                          style: TextStyle(
+                      /***ROW Started */
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            _response.main.temp.round().toString(),
+                            style: TextStyle(
+                                fontSize: 130,
+                                color: Color.fromRGBO(226, 243, 246, 1)),
+                          ),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Text(
+                                "\u00b0C",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 40,
+                                    color: Color.fromRGBO(226, 243, 246, 1)),
+                              ),
+                              //SizedBox(height: 5),
+                              Row(
+                                children: [
+                                  SvgPicture.asset("assets/high.svg",
+                                      color: Color.fromRGBO(226, 243, 246, 1),
+                                      height: 30,
+                                      width: 20),
+                                  Text(
+                                    _response.main.tempMax.round().toString() +
+                                        "\u00b0",
+                                    style: TextStyle(
+                                        fontSize: 20,
+                                        color:
+                                            Color.fromRGBO(226, 243, 246, 1)),
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  SvgPicture.asset("assets/low.svg",
+                                      color: Color.fromRGBO(226, 243, 246, 1),
+                                      height: 30,
+                                      width: 20),
+                                  Text(
+                                    _response.main.tempMin.round().toString() +
+                                        "\u00b0",
+                                    style: TextStyle(
+                                        fontSize: 20,
+                                        color:
+                                            Color.fromRGBO(226, 243, 246, 1)),
+                                  ),
+                                ],
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+
+                      /******COLUMN */
+                      Column(
+                        children: [
+                          Text(
+                            _response.name + ", " + _response.sys.country,
+                            style: TextStyle(
                               color: Color.fromRGBO(226, 243, 246, 1),
-                              fontSize: 30),
-                        ),
-                        Text(
-                          "Haze",
-                          style: TextStyle(
-                              color: Color.fromRGBO(226, 243, 246, 1),
-                              fontSize: 20),
-                        ),
-                        SvgPicture.asset(
-                          "assets/weather/cloudy.svg",
-                          color: Color.fromRGBO(226, 243, 246, 1),
-                          height: 200,
-                          width: 150,
-                        )
-                      ],
-                    )
-                  ],
-                )),
+                              fontSize: 25,
+                            ),
+                          ),
+                          Text(
+                            "Haze",
+                            style: TextStyle(
+                                color: Color.fromRGBO(226, 243, 246, 1),
+                                fontSize: 20),
+                          ),
+                          SvgPicture.asset(
+                            "assets/weather/cloudy.svg",
+                            color: Color.fromRGBO(226, 243, 246, 1),
+                            height: 200,
+                            width: 200,
+                          )
+                        ],
+                      )
+                    ],
+                  )),
+            ),
           );
         }
         return CircularProgressIndicator();
